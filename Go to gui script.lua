fca5c621364856ce53e9334f543d8e35dcf6e945
fnatagd <galikegor7@gmail.com>
2024-08-22 20:16:51 +0300

    Add files via upload

---
+++ Go to gui script.lua
@@ -0,0 +1,13 @@
+--[Library]--
+
+local Library =  loadstring(game:HttpGet("https://raw.githubusercontent.com/fnatagd/test-library-/main/testing.lua"))()
+
+--[screen of script]--
+
+local Frame1 = _G.Main:New("Go to gui/hub")
+
+--[Button]--
+
+Frame1:Button("name of your gui/hub",function()
+	  --[script here]--
+end)
